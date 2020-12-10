91a0b8c7c4b95147b32bc9d68cc15e0ab2b66636
P1561 <P1561@DESKTOP-0E8MCVH>
2020-12-10 17:44:13 +0300

    initial

---
+++ dotnetgRPC.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using dotnetgRPC.Protos;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnetgRPC.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var sensorClient = new SensorService.SensorServiceClient(channel);
+
+            var response = sensorClient.Sensor(new SensorDataRequest
+            {
+                Mac = "ED67385ADF54"
+            });
+
+            Console.WriteLine(response.RawData);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dotnetgRPC/Services/SensorService.cs
@@ -0,0 +1,22 @@
+﻿using dotnetgRPC.Protos;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetgRPC.Services
+{
+    public class SensorService : Protos.SensorService.SensorServiceBase
+    {
+        public override Task<SensorDataReply> Sensor(SensorDataRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Sensor Mac: { request.Mac}");
+
+            return Task.FromResult(new SensorDataReply
+            {
+                RawData = "02010408FFA55A000500000E4C5F"
+            });
+        }
+    }
+}
